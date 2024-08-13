04b4695f38ad8ac5a46e7594dc6ab5ef2c32dc31
d3npa <kyo@tilde.pink>
2024-08-14 08:18:09 +0900

    remove old commented code

--- src/tcpserver.rs
+++ src/tcpserver.rs
@@ -9,7 +9,6 @@ pub async fn listen(
 ) -> ! {
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
-    // let mut buf = [0; 4096];
 
     loop {
         let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
@@ -23,14 +22,6 @@ pub async fn listen(
 
         ctrl.gpio_set(0, true).await;
 
-        // if socket.write_all(b"[*] hello~\n").await.is_err() {
-        //     continue;
-        // }
-
-        // if socket.flush().await.is_err() {
-        //     continue;
-        // }
-
         if let Some(sensor) = temp_sensor::SENSOR.lock().await.as_mut() {
             sensor.send_start().await;
             let packet = sensor.read_response().await;
@@ -42,20 +33,6 @@ pub async fn listen(
                 packet.checksum,
             ];
 
-            // let mut buf_ascii = [0xa; 41];
-            // for (i, &b) in buf.iter().enumerate() {
-            //     let ascii = {
-            //         if b == 0 {
-            //             0x30
-            //         } else if b == 1 {
-            //             0x31
-            //         } else {
-            //             0xff
-            //         }
-            //     };
-            //     buf_ascii[i] = ascii;
-            // }
-
             if socket.write_all(&buf).await.is_err() {
                 continue;
             }
@@ -64,26 +41,5 @@ pub async fn listen(
                 continue;
             }
         }
-
-        // loop {
-        //     let n = match socket.read(&mut buf).await {
-        //         Ok(0) => break, // eof
-        //         Ok(n) => n,
-        //         Err(_e) => break,
-        //     };
-
-        //     buf[n] = 0;
-
-        //     // if let Ok(string) = crate::null_term_string(&buf) {
-        //     //     if string.is_empty() {
-        //     //         continue;
-        //     //     }
-
-        //     //     let status = handle_command(string.trim()).await;
-        //     //     if socket.write_all(&status.mesg).await.is_err() {
-        //     //         break;
-        //     //     }
-        //     // }
-        // }
     }
 }
